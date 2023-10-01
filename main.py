5132e42cbb84ef0995bba9bd3267ead831d7e97e
hutao <hutaovn@gmail.com>
2023-10-01 21:12:15 +0700

    Add gui (tkinter)

--- main.py
+++ main.py
@@ -1,24 +1,96 @@
 import asyncio
 import base64
 import json
-import msvcrt
 import os
 import signal
 import threading
 import time
 
-import colorama
 import psutil
 
 import requests
 import urllib3
+import tkinter as tk
+import win32api
 from requests import JSONDecodeError
 from requests.auth import HTTPBasicAuth
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # ChampPicker by l2stack
-artDecode = 'CiAgICBfX19fXyBfICAgICAgICAgICAgICAgICAgICAgICAgICBfX19fX18gXyAgICAgIF8KICAgLyAgX18gXCB8ICAgICAgICAgICAgICAgICAgICAgICAgIHwgX19fIChfKSAgICB8IHwKICAgfCAvICBcLyB8X18gICBfXyBfIF8gX18gX19fICBfIF9fIHwgfF8vIC9fICBfX198IHwgX19fX18gXyBfXwogICB8IHwgICB8ICdfIFwgLyBfYCB8ICdfIGAgXyBcfCAnXyBcfCAgX18vfCB8LyBfX3wgfC8gLyBfIFwgJ19ffAogICB8IFxfXy9cIHwgfCB8IChffCB8IHwgfCB8IHwgfCB8XykgfCB8ICAgfCB8IChfX3wgICA8ICBfXy8gfAogICAgXF9fX18vX3wgfF98XF9fLF98X3wgfF98IHxffCAuX18vXF98ICAgfF98XF9fX3xffFxfXF9fX3xffAogICAgICAgICAgICAgICAgICAgICAgICAgICAgfCB8CiAgICAgICAgICAgICAgICAgICAgICAgICAgICB8X3wgICAgICAgYnk6IGwyc3RhY2sKCiog4bqkbiBjw6FjIHBow61tIHTGsMahbmcg4bupbmcgdMOtbmggbsSDbmcgYsOqbiBkxrDhu5tpIMSR4buDIHPhu60gZOG7pW5nCgkxOiBDaOG7iSBwaWNrIHTGsOG7m25nCgkyOiBQaWNrIHbDoCBsb2NrIHTGsOG7m25nCgkzOiBNb2Rza2luICjEkGFuZyBwaMOhdCB0cmnhu4NuKQoJNDogVOG7sSDEkeG7mW5nIGNo4bqlcCBuaOG6rW4gdHLhuq1uIMSR4bqldQoJNTogxJDDs25nIOG7qW5nIGThu6VuZw=='
+# create window
+root = tk.Tk()
+
+# set title
+root.title("ChampPicker")
+
+# set background color
+root.configure(bg="#1e1e1e")
+
+# disable tool bar
+root.overrideredirect(True)
+
+# get screen size
+screen_width = win32api.GetSystemMetrics(0)
+screen_height = win32api.GetSystemMetrics(1)
+
+# calc pos to spawn window (center screen)
+window_width = 612
+window_height = 334
+x = (screen_width - window_width) // 2
+y = (screen_height - window_height) // 2
+
+# set window size and window pos
+root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+# set default offset for move event
+offset_x, offset_y = 0, 0
+
+
+def get_pos(event):
+    global offset_x, offset_y
+    offset_x = event.x
+    offset_y = event.y
+
+
+def move_window(event):
+    root.geometry(f"{window_width}x{window_height}+{event.x_root - offset_x}+{event.y_root - offset_y}")
+
+
+def create_text_label(pos_x: int, pos_y: int, size: int, message: str):
+    label = tk.Label(root, text=message, font=("monospace", size), fg="white", bg="#1e1e1e")
+    label.place(x=pos_x, y=pos_y)  # Margin top 5% và margin left 5%
+    label.config(anchor="w")  # anchor = lèft
+
+
+def create_button_label(text: str, font_size: int, btn_width: int, btn_height: int, pos_x: int, pos_y: int, color: str, command):
+    btn = tk.Button(root, text=text, command=command, font=("monospace", font_size), fg='white', bg=color,
+                            width=btn_width, height=btn_height,
+                            bd=5)
+    btn.place(x=pos_x, y=pos_y)
+    btn.config(cursor="hand2")
+
+
+# bind event
+root.bind("<Button-1>", get_pos)
+root.bind("<B1-Motion>", move_window)
+
+# app name label
+create_text_label(30, 30, 12, "ChampPicker v1.0.5")
+create_text_label(30, 60, 8, "author: l2stack")
+create_text_label(30, 90, 12, "Các tính năng chính (Ấn các nút tương ứng):")
+create_button_label("Chọn tướng", 12, 9, 0, 30, 120, 'green', None)
+
+
+# #
+# input_entry = tk.Entry(root, font=("monospace", 24), fg="#3cb371", justify="center", bd=0)
+# input_entry.place(x=30, y=80)  # Margin top 5% và margin left 5%
+# input_entry.config(width=21, insertbackground="white")  # Độ rộng, màu cursor và căn giữa văn bản
+
+# # Tạo button
+
+
+artDecode = 'ICAgX19fIF8gICAgICAgICAgICAgICAgICAgICAgICAgICAgIF9fXyBfICAgICAgXyAgICAgICAgICAgICAKICAvIF9fXCB8X18gICBfXyBfIF8gX18gX19fICBfIF9fICAgLyBfIChfKSBfX198IHwgX19fX18gXyBfXyAKIC8gLyAgfCAnXyBcIC8gX2AgfCAnXyBgIF8gXHwgJ18gXCAvIC9fKS8gfC8gX198IHwvIC8gXyBcICdfX3wKLyAvX19ffCB8IHwgfCAoX3wgfCB8IHwgfCB8IHwgfF8pIC8gX19fL3wgfCAoX198ICAgPCAgX18vIHwgICAKXF9fX18vfF98IHxffFxfXyxffF98IHxffCB8X3wgLl9fL1wvICAgIHxffFxfX198X3xcX1xfX198X3wgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgIHxffCAgICAgICBieTogbDJzc3RhY2sKTmjhuq1wIGPDoWMgcGjDrW0gdMawxqFuZyDhu6luZyDEkeG7gyBz4butIGThu6VuZyB0w61uaCBuxINuZwoxOiBDaOG7iSBwaWNrIHTGsOG7m25nCjI6IFBpY2sgdsOgIGxvY2sgdMaw4bubbmcKMzogQ2hhdCB24buLIHRyw60KNDogVOG7sSDEkeG7mW5nIGNo4bqlcCBuaOG6rW4gdHLhuq1uIMSR4bqldQo1OiDEkMOzbmcg4bupbmcgZOG7pW5n'
 pick_thread = False
 accept_thread = False
 leagueClientExe = "LeagueClient.exe"
@@ -95,6 +167,12 @@ def pick(id, champion_id, account, password, http, port):
     return not bool(response)
 
 
+def chat(message, account, password, http, port):
+    url = f'/lol-game-client-chat/v1/party-messages'
+    body = {'message': message}
+    request(url, account, password, http, port, 'POST', body)
+
+
 def _lock(id, account, password, http, port, ):
     url = f'/lol-champ-select/v1/session/actions/{id}/complete'
     request(url, account, password, http, port, 'POST')
@@ -133,13 +211,13 @@ def handler(inp):
         print('Không tìm thấy lock file')
         return
 
-    pick_lock = inp == b'2'
+    pick_lock = inp == '2'
     with open(lock_file, 'r') as f:
         content = f.read().split(":")
 
-    if inp == b'4' or inp == b'3':
+    if inp == '4' or inp == '3':
         message = None
-        if inp == b'3':
+        if inp == '3':
             message = input('Nhập tin nhắn cần gởi: ')
         threading.Thread(target=match_accept_thread, args=(content[4], content[3], content[2], message)).start()
         return
@@ -227,13 +305,15 @@ def kill():
     asyncio.run(os.kill(current_pid, signal.SIGTERM))  # type: ignore
 
 
-if __name__ == '__main__':
-    print(f"{colorama.Fore.YELLOW}{decode(artDecode)}")
+# Bắt sự kiện khi người dùng ấn các nút 1, 2, 3, 4, 5
+def key_pressed(event):
+    if event.char in ['1', '2', '3', '4', '5']:
+        if event.char == '5':
+            kill()
+        handler(event.char)
 
-    while True:
-        if msvcrt.kbhit():
-            key = msvcrt.getch()
-            if key in [b'1', b'2', b'4', b'5']:
-                if key == b'5':
-                    kill()
-                handler(key)
+
+root.bind("<Key>", key_pressed)
+
+# Chạy ứng dụng
+root.mainloop()
